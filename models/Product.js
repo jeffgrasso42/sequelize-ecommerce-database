007204cd8e7aa5c14773906e9bdd53fd3b3d2684
JAG <jagrasso42@gmail.com>
2022-11-03 16:57:20 -0400

    added foreign key to Product

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -24,6 +25,13 @@ Product.init(
       allowNull: false,
       validate: {
         isDecimal: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id',
       },
     },
   },
